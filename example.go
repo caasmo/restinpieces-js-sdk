d48cc4c00a470d815b3e641c22fef7165d2aab20
caasmo <226161+caasmo@users.noreply.github.com>
2025-04-07 21:18:46 +0200

    fmt

--- example.go
+++ example.go
@@ -1,25 +1,26 @@
 package main
+
 //go:generate go run gen/gogenerate-assets.go -baseDir static
 
 import (
-	"flag"
-	"log/slog"
-	"os"
 	"embed"
+	"flag"
 	"io/fs"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/caasmo/restinpieces"
 
 	//"github.com/caasmo/restinpieces/custom"
 
-    // TODO
-	r "github.com/caasmo/restinpieces/router"
+	// TODO
 	"github.com/caasmo/restinpieces/core"
+	r "github.com/caasmo/restinpieces/router"
 )
 
-// entry points is custom 
-// New creates app and 
+// entry points is custom
+// New creates app and
 // user says load auth routes
 // user can use the router
 // and has db conection to zombiezen or cranwsaw
@@ -32,10 +33,10 @@ import (
 // new pure dn jsut pool.
 // use has his own.
 // revisitar si necesitamos db, auth endpoints estan ocultos,b
-// db is auth db , only needed for auth, 
+// db is auth db , only needed for auth,
 // we needed interface for auth with various
 // pass the pool to db.
-// if you want to use auth pass    
+// if you want to use auth pass
 // User: i want to work with zombiezen, auth need pool
 // pass pool to db,
 // reanme to Dbauth, build dbauth with pool
@@ -53,19 +54,19 @@ import (
 // user has to build app.
 //
 // restinpieces.New is the custom
-// user say: 
+// user say:
 // fefault
 // restinpieces.New(
-	// withZombiezenDbClient
-	// WithStandardMux
-	// 
-// how i use the router 
+// withZombiezenDbClient
+// WithStandardMux
+//
+// how i use the router
 // and the db
 // how i use the queue
 // how i disable the endpoints
 // framework has no command make you own
 //)
-// split db interface in dbAuth and dbQueue, 
+// split db interface in dbAuth and dbQueue,
 // user can use dbQueue is generic enough
 // restinpieces is the struct.
 // has the server.
@@ -76,14 +77,12 @@ import (
 // user has own struct with its own db
 // can embed the restinpieces object
 // config user uses its own config
-// db we wnat the pool, the dbclient 
-// dbinterface has get pool Pool() 
+// db we wnat the pool, the dbclient
+// dbinterface has get pool Pool()
 // you can have your own and pass to db, or lets start with dbBase SetPool(), Pool()
 
-
-
 var (
-	dbfile  = flag.String("db", "app.db", "Path to the database file")
+	dbfile = flag.String("db", "app.db", "Path to the database file")
 )
 
 //go:embed static/dist/*
@@ -102,7 +101,7 @@ func main() {
 	app, srv, err := restinpieces.New(
 		*dbfile,
 		restinpieces.WithDBCrawshaw(*dbfile), // Pass dbfile here as well for DB init
-		restinpieces.WithRouterServeMux(),  // Using Httprouter as an example default
+		restinpieces.WithRouterServeMux(),    // Using Httprouter as an example default
 		restinpieces.WithCacheRistretto(),
 		restinpieces.WithTextLogger(nil), // Use default text logger options
 	)
@@ -115,7 +114,7 @@ func main() {
 	// Serve static files from configured public directory
 
 	// --- file server ---
-    cfg:= app.Config()
+	cfg := app.Config()
 	subFS, err := fs.Sub(EmbeddedAssets, cfg.PublicDir)
 	if err != nil {
 		// TODO
@@ -134,9 +133,8 @@ func main() {
 	// Note: config is now accessed via app.Config()
 	//logEmbeddedAssets(restinpieces.EmbeddedAssets, app.Config(), app.Logger())
 
-//		app.Logger().Info("Starting server in verbose mode")
+	//		app.Logger().Info("Starting server in verbose mode")
 
 	// Start the server
 	srv.Run() // srv is returned by restinpieces.New
 }
-
